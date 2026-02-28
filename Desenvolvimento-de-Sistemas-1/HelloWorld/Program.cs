82ae543b2beb2ee07fda75a26f5f626fb202dd10
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2026-02-28 20:35:02 -0300

    feat: add basic console app and gitignore

---
+++ Desenvolvimento-de-Sistemas-1/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World, I'm Túlio Thauã Dutra!");
+        DateTime dataCorrente = DateTime.Now;
+        Console.WriteLine("Data/Hora: " + dataCorrente);
+    }
+}
