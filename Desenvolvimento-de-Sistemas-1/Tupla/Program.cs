d6abb2b61def64d03562ce55f1d5a516b57223b8
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2026-03-01 15:49:29 -0300

    feat: add new console app - using the tupla method

---
+++ Desenvolvimento-de-Sistemas-1/Tupla/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        (string Nome, int Idade) pessoa = ("Túlio Thauã Dutra", 18);
+        Console.WriteLine($"{pessoa.Nome}, você tem {pessoa.Idade} anos.");
+    }
+}
