703a81db492851dfca1224cb99bebf773ada7c01
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2026-03-01 17:40:18 -0300

    feat: allow user to input grade values via console

--- Desenvolvimento-de-Sistemas-1/MediaFinal/Program.cs
+++ Desenvolvimento-de-Sistemas-1/MediaFinal/Program.cs
@@ -2,13 +2,22 @@
 {
     private static void Main(string[] args)
     {   
-        var aluno = "Túlio";
-        var nota1 = 7.5; 
-        var nota2 = 8.0;
-        var nota3 = 6.0;
-        var nota4 = 9.0;
-        var media = (nota1 + nota2 + nota3 + nota4) / 4;
-        Console.WriteLine($"Aluno: {aluno}");
-        Console.WriteLine($"A média das notas é: {media}");
+        int nota1, nota2, nota3, nota4;
+        decimal media;
+
+        Console.WriteLine("Nome do Aluno:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Nota 1:");
+        nota1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nota 2:");
+        nota2 = int.Parse(Console.ReadLine());  
+        Console.WriteLine("Nota 3:");
+        nota3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nota 4:");
+        nota4 = int.Parse(Console.ReadLine());
+
+        media = (decimal)(nota1 + nota2 + nota3 + nota4) / 4;
+        Console.WriteLine($"A média do aluno {nome} é: {media}");
     }
 }
